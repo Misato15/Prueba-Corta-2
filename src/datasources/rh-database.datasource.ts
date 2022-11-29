3b764db6c13c8edd93f3bee81c07e475c43842be
Misato15 <salvadorchinchilla03@gmail.com>
2022-11-28 18:55:25 -0600

    segundo commit, datasource

---
+++ src/datasources/rh-database.datasource.ts
@@ -0,0 +1,32 @@
+import {inject, lifeCycleObserver, LifeCycleObserver} from '@loopback/core';
+import {juggler} from '@loopback/repository';
+
+const config = {
+  name: 'rh_database',
+  connector: 'mongodb',
+  url: 'mongodb+srv://admin:taehyun@cluster0.irewiil.mongodb.net/candidatos',
+  host: 'cluster0.irewiil.mongodb.net',
+  port: 27018,
+  user: '',
+  password: '',
+  database: 'rh_database',
+  useNewUrlParser: true
+};
+
+// Observe application's life cycle to disconnect the datasource when
+// application is stopped. This allows the application to be shut down
+// gracefully. The `stop()` method is inherited from `juggler.DataSource`.
+// Learn more at https://loopback.io/doc/en/lb4/Life-cycle.html
+@lifeCycleObserver('datasource')
+export class RhDatabaseDataSource extends juggler.DataSource
+  implements LifeCycleObserver {
+  static dataSourceName = 'rh_database';
+  static readonly defaultConfig = config;
+
+  constructor(
+    @inject('datasources.config.rh_database', {optional: true})
+    dsConfig: object = config,
+  ) {
+    super(dsConfig);
+  }
+}
